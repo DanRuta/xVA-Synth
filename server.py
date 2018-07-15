e6dfef5d6f1c20bd9ac3d9c6a836f746fe13f760
Dan Ruta <ruta_dan_sebastian@yahoo.co.uk>
2018-07-15 14:23:12 +0100

    Added python server logging

--- server.py
+++ server.py
@@ -1,13 +1,27 @@
 
 import eval
+import logging
 import json
+import traceback
 from sys import argv
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 print("Start")
 
 model = 0
-loaded = False
+
+logger = logging.getLogger('serverLog')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('server.log')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+logger.info("New session")
 
 class Handler(BaseHTTPRequestHandler):
     def _set_response(self):
@@ -16,28 +30,35 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        returnString = "[DEBUG] Get request for {}".format(self.path).encode("utf-8")
+        logger.info(returnString)
         self._set_response()
-        self.wfile.write("[DEBUG] Get request for {}".format(self.path).encode("utf-8"))
+        self.wfile.write(returnString)
 
     def do_POST(self):
+        try:
+            global model
+            logger.info("POST {}".format(self.path))
 
-        global model
+            content_length = int(self.headers['Content-Length'])
+            post_data = json.loads(self.rfile.read(content_length).decode('utf-8'))
 
-        content_length = int(self.headers['Content-Length'])
-        post_data = json.loads(self.rfile.read(content_length).decode('utf-8'))
+            print("POST")
+            print(self.path)
+            print(post_data)
+            logger.info(post_data)
 
-        print("POST")
-        print(self.path)
-        print(post_data)
+            if self.path == "/loadModel":
+                model = eval.loadModel(model=post_data["model"], outputs=post_data["outputs"], cmudict=post_data["cmudict"])
 
-        if self.path == "/loadModel":
-            model = eval.loadModel(model="model.ckpt-{}".format(post_data["model"]), outputs=post_data["outputs"], cmudict=post_data["cmudict"])
+            if self.path == "/synthesize":
+                eval.syntesize(model, list(map(int, post_data["sequence"].split(","))), post_data["outfile"])
 
-        if self.path == "/synthesize":
-            eval.syntesize(model, list(map(int, post_data["sequence"].split(","))), post_data["outfile"])
-
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+            self._set_response()
+            self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        except Exception as e:
+            logger.info("Post Error:\n {}".format(repr(e)))
+            logger.info(traceback.format_exc())
 
 server = HTTPServer(("",8008), Handler)
 try:
