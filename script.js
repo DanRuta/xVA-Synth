20d4c8403736d5b322ae4ffe106c609cad5f0331
Dan Ruta <ruta_dan_sebastian@yahoo.co.uk>
2018-08-01 13:54:18 +0100

    Removed question mark from saved file names, to avoid errors

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ generateVoiceButton.addEventListener("click", () => {
         const voiceType = title.dataset.modelId
 
         const sequence = text_to_sequence(dialogueInput.value).join(",")
-        const outputFileName = dialogueInput.value.slice(0, 50)
+        const outputFileName = dialogueInput.value.slice(0, 50).replace("?", "")
 
         try {fs.unlinkSync(localStorage.getItem("tempFileLocation"))} catch (e) {/*Do nothing*/}
 
