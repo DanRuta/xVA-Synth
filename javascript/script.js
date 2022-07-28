cb4a4bc5e477a27900f52255cd802d2fe48d5b6a
Dan Ruta <ruta_dan_sebastian@yahoo.co.uk>
2022-07-28 13:17:16 +0100

    v2.3.0 version bump

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 "use strict"
-window.appVersion = "v2.2.1"
+window.appVersion = "v2.3.0"
 
 window.PRODUCTION = module.filename.includes("resources")
 const path = window.PRODUCTION ? "./resources/app" : "."
